c00275623cc055f4f38fea8ee6ca8c7ca1d9c170
Arslan <91959072+rasulow@users.noreply.github.com>
2024-11-06 19:25:07 +0500

    products filter frontend, product-detail

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/products/', views.products, name='products'),
+    path('/product-detail/', views.product_detail, name='product-detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def products(request):
+    return render(request, 'pages/products.html')
+
+
+def product_detail(request):
+    return render(request, 'pages/product-detail.html')

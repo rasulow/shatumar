e0d0cb64a2107e3f5f737b63aa1f439c159b360f
Arslan <91959072+rasulow@users.noreply.github.com>
2024-11-13 21:13:45 +0500

    adventure

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('products/', views.ProductsView.as_view(), name='products'),
-    path('product-detail/', views.ProductsView.as_view(), name='product-detail'),
+    path('products', views.ProductsView.as_view(), name='products'),
+    path('product/<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
     path('contact/', views.ContactView.as_view(), name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.db.models import Count, OuterRef, Subquery
 from django.core.cache import cache
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . import models
 
 class IndexView(TemplateView):
@@ -45,9 +46,61 @@ class IndexView(TemplateView):
 class ProductsView(TemplateView):
     template_name = 'pages/products.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        categories = cache.get_or_set('categories', lambda: models.Category.objects.all().annotate(product_count=Count('product')), 300)
+        
+        products = cache.get_or_set('products', lambda: models.Product.objects.all().select_related('category').annotate(
+            img=Subquery(
+                models.ProductImage.objects.filter(
+                    product=OuterRef('id')
+                ).values('img')[:1]
+            )
+        ), 300)
+        
+        category_slug = self.request.GET.get('category', None)
+        if category_slug:
+            products = products.filter(category__slug=category_slug)
+        
+        
+        search = self.request.GET.get('search', None)
+        if search:
+            products = products.filter(name__icontains=search)
+        paginator = Paginator(products, 9)
+        page_number = self.request.GET.get('page')
+        
+        try:
+            products_page = paginator.get_page(page_number)
+        except (PageNotAnInteger, EmptyPage):
+            products_page = paginator.page(1 if not page_number else paginator.num_pages)
 
+        context['categories'] = categories
+        context['products'] = products_page
+        
+        galleries = cache.get_or_set('galleries', lambda: models.Gallery.objects.filter(is_active=True), 300)
+        context['galleries'] = galleries
+        
+        return context
+        
+        
 class ProductDetailView(TemplateView):
     template_name = 'pages/product-detail.html'
+    
+    def get_context_data(self, slug, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        product = cache.get_or_set(f'product_{slug}', lambda: models.Product.objects.get(slug=slug))
+        context['product'] = product
+        
+        images = cache.get_or_set(f'images_{slug}', lambda: models.ProductImage.objects.filter(product=product))
+        context['images'] = images
+        
+        galleries = cache.get_or_set('galleries', lambda: models.Gallery.objects.filter(is_active=True), 300)
+        context['galleries'] = galleries
+        
+        return context
+    
 
 
 class ContactView(TemplateView):

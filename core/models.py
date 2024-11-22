ec6f9e5921a2190e291174535b8c4cd2729cbb92
Arslan <91959072+rasulow@users.noreply.github.com>
2024-11-22 22:07:42 +0500

    commit

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ import io
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name=_('Category'))
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
     description = models.TextField(blank=True, null=True, verbose_name=_('Description'))
     img = models.ImageField(blank=True, null=True, upload_to='category_images/')
     slug = models.SlugField(max_length=255, unique=True)
@@ -52,8 +52,8 @@ class Category(models.Model):
         
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+    description = models.TextField(blank=True, null=True, verbose_name=_('Description'))
     slug = models.SlugField(max_length=255, unique=True)
     available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -89,7 +89,7 @@ class ProductImage(models.Model):
         
         
 class Banner(models.Model):
-    title = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('Title'))
     img = models.ImageField(upload_to='banners/')
     description = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
@@ -130,7 +130,7 @@ class Banner(models.Model):
         
         
 class Gallery(models.Model):
-    title = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('Title'))
     img = models.ImageField(upload_to='gallery/')
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -170,7 +170,7 @@ class Gallery(models.Model):
         
         
 class Certificates(models.Model):
-    title = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('Title'))
     img = models.ImageField(upload_to='certificates/')
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- core/translation.py
+++ core/translation.py
@@ -2,10 +2,18 @@ from modeltranslation.translator import translator, TranslationOptions
 from . import models
 
 
-class CategoryTranslationOptions(TranslationOptions):
+class BaseTranslationOptions(TranslationOptions):
     fields = ('name', 'description')    
     
+
+class BannerTranslationOptions(TranslationOptions):
+    fields = ('title',)
     
-translator.register(models.Category, CategoryTranslationOptions)
+
+translator.register(models.Category, BaseTranslationOptions)
+translator.register(models.Product, BaseTranslationOptions)
+translator.register(models.Banner, BannerTranslationOptions)
+translator.register(models.Gallery, BannerTranslationOptions)
+translator.register(models.Certificates, BannerTranslationOptions)
 
 
